191ca6e0e3ed6c70d4eee2ae8dc27ea0b223c348
blgkv1 <corinencoandrij@gmail.com>
2025-11-08 15:38:03 +0100

    use axios to fetch the data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,13 @@
+import axios from "axios";
 import Header from "../components/Header";
 import "./HomePage.css";
 import { products } from "../../starting-code/data/products";
 
 function HomePage() {
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <title>Ecommerce project</title>
